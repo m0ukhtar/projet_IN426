3b00c3ce19162902ec742b79787a339b3c95e0de
Moukhtar <99501355+m0ukhtar@users.noreply.github.com>
2025-05-21 09:32:54 +0000

    Add files via upload

---
+++ motion_robot_2_launch.py
@@ -0,0 +1,15 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package = "in426_motion",
+            executable = "motion_robot_2.py",
+            parameters = [os.path.join(get_package_share_directory("in426_simu"), "config", "ik_params.yaml")]
+        )
+    ])
